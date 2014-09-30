8e54f41c4c7b5b346dd08ee3bac3f3d7b66be65c
maxionwang <vegito.wang@gmail.com>
2014-09-29 23:47:29 -0500

    Purple

--- App/WeightConversions/gen/com/weight/conversions/R.java
+++ App/WeightConversions/gen/com/weight/conversions/R.java
@@ -1080,6 +1080,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int MainLayer=0x7f05003d;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,12 +1092,14 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int checkBox1=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int creatorName=0x7f05003f;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1113,12 +1116,14 @@ containing a value of this type.
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
+        public static final int nameText=0x7f05003e;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int randomLayer=0x7f05003c;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1140,6 +1145,9 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int versionText=0x7f050043;
+        public static final int weightPrompt=0x7f050042;
+        public static final int weightText=0x7f050040;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1241,7 +1249,14 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int checkMetrics=0x7f0a0013;
+        public static final int creatorName=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
+        public static final int titleText=0x7f0a0010;
+        public static final int versionAlpha=0x7f0a0016;
+        public static final int weight=0x7f0a0012;
+        public static final int weightPrompt=0x7f0a0014;
+        public static final int weightPromptHint=0x7f0a0015;
     }
     public static final class style {
         /** 
